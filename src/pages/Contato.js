b5d30090c814c5e2b3b332ff63f883caf50f144f
adriana oliveira luz <niltonribeirojr7@gmail.com>
2020-03-22 21:29:58 -0300

    inclui

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -1,17 +1,19 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const NotFoundPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <Link to="/">Go back to the homepage</Link>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    
+    <SEO title="404: Not found" />
+    <h1>Informações de Contato</h1>
+    <h2>Gostaríamos muito de auxiliar na preparação de sua próxima viagem. Para nos ajudar a ter uma ideia do que você precisa, preencha este formulário de contato.</h2>
+    <h3>Rua Jorge Angel Livraga, 154. Sl. 301
+Belo Horizonte - MG
+Estado e CEP 31155-420 E-mail: atendimento@goliveturismo.com.br
+Telefone: 55-31-3504-3686</h3>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
 
-export default SecondPage
+export default NotFoundPage
